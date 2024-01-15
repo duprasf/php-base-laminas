d9bae30bc3d561b4e58744036aeb742b83df6164
Francois Dupras <francois.dupras@hc-sc.gc.ca>
2024-01-15 11:28:08 -0500

    Uses LAMINAS_ROOT_PATH instead of __DIR__ in case your app requires this
    also replaced the old page-builder.php from ZF2 with the laminas version

--- code/public/index.php
+++ code/public/index.php
@@ -6,11 +6,12 @@ use Laminas\Mvc\Application;
 use Laminas\Stdlib\ArrayUtils;
 
 
+$root = getenv('LAMINAS_ROOT_PATH') ?: dirname(__DIR__);//'/var/www';
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
-chdir(dirname(__DIR__));
+chdir($root);
 
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
@@ -22,8 +23,7 @@ if (php_sapi_name() === 'cli-server') {
 }
 
 // Composer autoloading
-include __DIR__ . '/../vendor/autoload.php';
-
+include $root.'/vendor/autoload.php';
 if (! class_exists(Application::class)) {
     throw new RuntimeException(
         "Unable to load application.\n"
@@ -34,9 +34,9 @@ if (! class_exists(Application::class)) {
 }
 
 // Retrieve configuration
-$appConfig = require __DIR__ . '/../config/application.config.php';
-if (file_exists(__DIR__ . '/../config/development.config.php')) {
-    $appConfig = ArrayUtils::merge($appConfig, require __DIR__ . '/../config/development.config.php');
+$appConfig = require $root.'/config/application.config.php';
+if (file_exists($root.'/config/development.config.php')) {
+    $appConfig = ArrayUtils::merge($appConfig, require $root.'/config/development.config.php');
 }
 
 // Run the application!
