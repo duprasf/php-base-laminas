a6a8829074643be7be0d097c00cd3bfc7a4338a4
Francois Dupras <francois.dupras@hc-sc.gc.ca>
2024-03-06 11:23:50 -0500

    Adding missing template for login-en and login-fr in UserAuth module

--- code/module/UserAuth/config/autoload/gc-notify.global.php
+++ code/module/UserAuth/config/autoload/gc-notify.global.php
@@ -1,13 +1,16 @@
 <?php
+
 namespace UserAuth;
 
 return [
-    'gc-notify-config'=>[
-        __NAMESPACE__=>[
-            'appName'=>'UserAuth',
-            'templates'=>[
-                'confirm-email'=>'a3e9adf7-52c4-47b8-8ecc-834bb0695157',
-                'reset-password'=>'56523a2f-9e5c-4f30-84c0-3d97ef0a945b',
+    'gc-notify-config' => [
+        __NAMESPACE__ => [
+            'appName' => 'Generic authentication (please update the config)',
+            'templates' => [
+                'confirm-email' => 'a3e9adf7-52c4-47b8-8ecc-834bb0695157',
+                'reset-password' => '56523a2f-9e5c-4f30-84c0-3d97ef0a945b',
+                'login-en'=>'f601d4ae-0e38-4a31-8eb9-9b76e34dec46',
+                'login-fr'=>'fa2314e9-e51c-4fbd-a70d-6b684bc7f043',
             ],
         ],
     ],
