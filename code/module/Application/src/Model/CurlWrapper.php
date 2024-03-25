0eb49b8c64bb7aeaa9d9a9aae3690774cf1b20e8
Francois Dupras <francois.dupras@hc-sc.gc.ca>
2024-03-25 19:46:57 -0400

    added cURL Wrapper class (CurlWrapper)

---
+++ code/module/Application/src/Model/CurlWrapper.php
@@ -0,0 +1,264 @@
+<?php
+namespace Application\CurlWrapper;
+
+use Application\Exception\CurlException;
+
+
+class CurlWrapper
+{
+    private $handle;
+    private $verb;
+    private $payload;
+    private $lastPage;
+    private $executed;
+    private $returnPage;
+    private $attachedFiles;
+
+    public const GET='GET';
+    public const POST='POST';
+    public const PUT='PUT';
+    public const DELETE='DELETE';
+    public const HEAD='HEAD';
+    public const OPTIONS='OPTIONS';
+    public const PATCH='PATCH';
+
+    public function __construct(?string $url=null, ?Iterable $options=null)
+    {
+        $this->handle = curl_init($url);
+        $this->reset();
+
+        if($options) {
+            $this->setOptions($options);
+        }
+    }
+
+    public function __destruct()
+    {
+        curl_close($this->handle);
+    }
+
+    public function __clone()
+    {
+        $this->handle = curl_copy_handle($this->handle);
+    }
+
+    public function __call($name, $arg)
+    {
+        if($name == 'verb') {
+            $name = array_shift($arg);
+        }
+
+        switch($name) {
+            case 'url':
+                curl_setOpt($this->handle, CURLOPT_URL, $arg[0]);
+                break;
+            case self::GET:
+            case self::POST:
+            case self::PUT:
+            case self::DELETE:
+            case self::HEAD:
+            case self::OPTIONS:
+            case self::PATCH:
+                $this->verb=$name;
+                if(isset($arg[0])) {
+                    $this->payload=$arg[0];
+                }
+                break;
+            case 'getCode':
+            case 'getStatus':
+            case 'getHttpCode':
+            case 'getResponseCode':
+                return $this->getHttpResponseStatusCode();
+                break;
+        }
+        return $this;
+    }
+
+    public function setOptions(Iterable $options)
+    {
+        if(!curl_setopt_array($this->handle, $options)) {
+            throw new CurlException('One or more options could not be set '.$this->getLastError());
+        }
+        return $this;
+    }
+
+    public function setOption(int $option, $value)
+    {
+        if(!curl_setopt($this->handle, $option, $value)) {
+            throw new CurlException('Option could not be set '.curl_error($this->handle));
+        }
+        return $this;
+    }
+
+    public function doNotReturnPage(bool $bool)
+    {
+        $this->returnPage=$bool;
+        return $this;
+    }
+
+    public function credentials($username, $password)
+    {
+        curl_setopt($this->handle, CURLOPT_USERPWD, "$username:$password");
+        return $this;
+    }
+
+    public function credential($username, $password)
+    {
+        return $this->credentials($username, $password);
+    }
+
+    public function headers(array $headers)
+    {
+        curl_setopt($this->handle, CURLOPT_HTTPHEADER, $headers);
+        return $this;
+    }
+
+    public function headersJson()
+    {
+        curl_setopt($this->handle, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Accept: application/json',
+        ]);
+        return $this;
+    }
+
+    public function addFile(string $filename, ?string $mime_type = null, ?string $posted_filename = null)
+    {
+        // should add validation of location of the file...
+        $this->attachedFiles[] = new CURLFile($filename,$mime_type,$posted_filename);
+        return $this;
+    }
+
+    public function exec()
+    {
+        $includePayload=false;
+        switch($this->verb) {
+            case self::POST:
+                curl_setopt($this->handle, CURLOPT_POST, true);
+                $includePayload=true;
+                break;
+            case self::PUT:
+            case self::PATCH:
+                curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, $this->verb);
+                $includePayload=true;
+                break;
+            case self::DELETE:
+            case self::OPTIONS:
+            case self::HEAD:
+                curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, $this->verb);
+                break;
+            case self::GET:
+            default:
+                curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, $this->verb);
+                $includePayload=true;
+                break;
+        }
+
+        if(count($this->attachedFiles)) {
+            if(is_null($this->payload)) {
+                $this->payload=[];
+            } else if(!is_array($this->payload)) {
+                $this->payload=json_decode($this->payload, true);
+            }
+            if(!is_array($this->payload)) {
+                throw new CurlException('The payload could not be converted to an array');
+            }
+            foreach($this->attachedFiles as $file) {
+                if(!file_exists($file->getFilename())) {
+                    throw new CurlException('File does not exists ', $file->getFilename());
+                }
+                $this->payload[$file->getPostFilename()]=$file;
+            }
+            $includePayload = true;
+        }
+        if($includePayload && $this->payload) {
+            curl_setopt($this->handle, CURLOPT_POSTFIELDS, $this->payload);
+        }
+
+        if($this->returnPage) {
+            curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, true);
+        }
+
+        $this->lastPage = curl_exec($this->handle);
+        $this->executed=true;
+        return $this;
+    }
+
+    public function getReturn()
+    {
+        return $this->getPage();
+    }
+
+    public function getPage()
+    {
+        if(!$this->executed) {
+            throw new CurlException('You need to execute the CURL before fetching this information');
+        }
+        return $this->lastPage;
+    }
+
+    public function getReturnJson()
+    {
+        return $this->getPageJson();
+    }
+
+    public function getPageJson()
+    {
+        return json_decode($this->getPage(), true);
+    }
+
+    public function getReturnCode()
+    {
+        if(!$this->executed) {
+            throw new CurlException('You need to execute the CURL before fetching this information');
+        }
+        return $this->getInfo(CURLINFO_HTTP_CODE);
+    }
+
+    public function getInfo(?int $opt = null)
+    {
+        return curl_getinfo($this->handle, $opt);
+    }
+
+    public function getHttpResponseStatusCode()
+    {
+        return $this->getInfo(CURLINFO_HTTP_CODE);
+    }
+
+    public function getUrl()
+    {
+        return $this->getInfo(CURLINFO_EFFECTIVE_URL);
+    }
+
+    public function reset()
+    {
+        curl_reset($this->handle);
+        $this->verb=null;
+        $this->payload=null;
+        $this->lastPage=null;
+        $this->executed=false;
+        $this->returnPage=true;
+        $this->attachedFiles=[];
+        return $this;
+    }
+
+    public function getErrorNumber(): int
+    {
+        return curl_errno($this->handle);
+    }
+
+    public function getError(): ?string
+    {
+        return curl_error($this->handle);
+    }
+
+    public function getErrorStringFromNumber(int $errornum): ?string
+    {
+        return curl_strerror($errornum);
+    }
+
+    public function getVersion(): array
+    {
+        return curl_version();
+    }
+}
