bf7bd8b045b960b0d437227603543fc93f6b82c2
Francois Dupras <francois.dupras@hc-sc.gc.ca>
2024-06-06 20:14:53 -0400

    added condition in case the code is ran from CLI

--- code/module/Application/src/Application/Factory/DomainFactory.php
+++ code/module/Application/src/Application/Factory/DomainFactory.php
@@ -8,6 +8,7 @@ class DomainFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
-        return $container->get('Request')->getHeaders('host')->getFieldValue();
+        $header = $container->get('Request')->getHeaders('host');
+        return $header ? $header->getFieldValue() : '';
     }
 }
