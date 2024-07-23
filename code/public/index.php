86a5bb861a848d9e9516a82813b5c71ef62102f0
Francois Dupras <francois.dupras@hc-sc.gc.ca>
2024-07-23 11:49:14 -0400

    Errors were not always display if Laminas did not start correctly, so added error reporting if in dev

--- code/public/index.php
+++ code/public/index.php
@@ -5,42 +5,47 @@ declare(strict_types=1);
 use Laminas\Mvc\Application;
 use Laminas\Stdlib\ArrayUtils;
 
+if(getenv('PHP_DEV_ENV')) {
+    ini_set('display_errors', true);
+    error_reporting(E_ALL);
+}
+
 try {
-$root = getenv('LAMINAS_ROOT_PATH') ?: dirname(__DIR__);//'/var/www';
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
-chdir($root);
+    $root = getenv('LAMINAS_ROOT_PATH') ?: dirname(__DIR__);//'/var/www';
+    /**
+     * This makes our life easier when dealing with paths. Everything is relative
+     * to the application root now.
+     */
+    chdir($root);
 
-// Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server') {
-    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-    if (is_string($path) && __FILE__ !== $path && is_file($path)) {
-        return false;
+    // Decline static file requests back to the PHP built-in webserver
+    if (php_sapi_name() === 'cli-server') {
+        $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+        if (is_string($path) && __FILE__ !== $path && is_file($path)) {
+            return false;
+        }
+        unset($path);
     }
-    unset($path);
-}
 
-// Composer autoloading
-include $root.'/vendor/autoload.php';
-if (! class_exists(Application::class)) {
-    throw new RuntimeException(
-        "Unable to load application.\n"
-        . "- Type `composer install` if you are developing locally.\n"
-        . "- Type `vagrant ssh -c 'composer install'` if you are using Vagrant.\n"
-        . "- Type `docker-compose run laminas composer install` if you are using Docker.\n"
-    );
-}
+    // Composer autoloading
+    include $root.'/vendor/autoload.php';
+    if (! class_exists(Application::class)) {
+        throw new RuntimeException(
+            "Unable to load application.\n"
+            . "- Type `composer install` if you are developing locally.\n"
+            . "- Type `vagrant ssh -c 'composer install'` if you are using Vagrant.\n"
+            . "- Type `docker-compose run laminas composer install` if you are using Docker.\n"
+        );
+    }
 
-// Retrieve configuration
-$appConfig = require $root.'/config/application.config.php';
-if (file_exists($root.'/config/development.config.php')) {
-    $appConfig = ArrayUtils::merge($appConfig, require $root.'/config/development.config.php');
-}
+    // Retrieve configuration
+    $appConfig = require $root.'/config/application.config.php';
+    if (file_exists($root.'/config/development.config.php')) {
+        $appConfig = ArrayUtils::merge($appConfig, require $root.'/config/development.config.php');
+    }
 
-// Run the application!
-Application::init($appConfig)->run();
+    // Run the application!
+    Application::init($appConfig)->run();
 
 } catch(\Exception $e) {
     if(getenv('PHP_DEV_ENV')) {
